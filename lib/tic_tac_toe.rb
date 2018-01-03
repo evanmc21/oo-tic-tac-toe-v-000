e7014f7ab30ebb257d9e48bfa0fd6b59def25094
Evan Candler <emcandler21@gmail.com>
2018-01-03 20:49:00 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,7 +14,7 @@ class TicTacToe
     [1,4,7]
   ]
 
-  def display_board
+def display_board
  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
  puts "-----------"
  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
